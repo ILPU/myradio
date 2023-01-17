8c61b82b5f03136f9d45957d0a4e18a7cdc1f710
ILPU <kolochkov.andrew@gmail.com>
2023-01-17 13:47:01 +0300

    Add files via upload

---
+++ res/resource.h
@@ -0,0 +1,7 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDR_SKIN_DEFAU1                         100
+#define DLG_SETTINGS                            103
+#define IDC_TAB1                                1009
